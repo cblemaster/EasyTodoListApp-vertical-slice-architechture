b66b8eeb5b0f184c02a676bd2194caf9da56994c
Brian LeMaster <brian.lemaster@lcs.com>
2024-12-30 13:02:38 -0500

    made setters private and added public 'set mthods to todo entity

--- EasyTodoListApp/Domain/Todo.cs
+++ EasyTodoListApp/Domain/Todo.cs
@@ -3,11 +3,11 @@ namespace EasyTodoListApp.Domain;
 
 public class Todo : Entity<Todo>
 {
-   public Descriptor Description { get; init; } = default!;
-   public DateOnly? DueDate { get; init; }
-   public bool IsImportant { get; init; }
-   public bool IsComplete { get; init; }
-   public DateTimeStamps Dates { get; init; } = default!;
+   public Descriptor Description { get; private set; } = default!;
+   public DateOnly? DueDate { get; private set; }
+   public bool IsImportant { get; private set; }
+   public bool IsComplete { get; private set; }
+   public DateTimeStamps Dates { get; private set; } = default!;
    public override Identifier<Todo> Identifier { get; init; } = default!;
 
    private Todo() { }
@@ -22,7 +22,11 @@ public class Todo : Entity<Todo>
       Identifier = Identifier<Todo>.Create(Guid.NewGuid());
    }
 
-   public static Todo Create(string description, DateOnly? dueDate, bool isImportant, bool isComplete) =>new(description, dueDate, isImportant, isComplete);
+   public static Todo Create(string description, DateOnly? dueDate, bool isImportant, bool isComplete) => new(description, dueDate, isImportant, isComplete);
+   public void SetDescription(string description) => Description = Descriptor.CreateOrThrowArgException(description, Description.IsRequired, Description.IsAllowAllWhitespace, Description.MaxLength);
+   public void SetDueDate(DateOnly? dueDate) => DueDate = dueDate;
+   public void SetIsComplete(bool isComplete) => IsComplete = isComplete;
+   public void SetIsImportant(bool isImportant) => IsImportant = isImportant;
 
    public const int MAX_LENGTH_FOR_DESCRIPTION = 100;
    public const bool IS_DSCRIPTION_REQUIRED = true;

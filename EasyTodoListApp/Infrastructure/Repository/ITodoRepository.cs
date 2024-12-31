c5be38ff2c57cf5b36f9b45292ccb09f690d841a
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-30 19:06:56 -0500

    added repo interface

---
+++ EasyTodoListApp/Infrastructure/Repository/ITodoRepository.cs
@@ -0,0 +1,11 @@
+﻿using EasyTodoListApp.Domain;
+
+namespace EasyTodoListApp.Infrastructure.Repository
+{
+    public interface ITodoRepository
+    {
+        IEnumerable<Todo> GetAllTodosComplete();
+        IEnumerable<Todo> GetAllTodosNotComplete();
+        Task<Todo?> GetTodoByIdOrNullAsync(Identifier<Todo> id);
+    }
+}

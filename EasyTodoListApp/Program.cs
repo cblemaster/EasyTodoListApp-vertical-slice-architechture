f4c0062f9f66ec1452694327d2c998b2f83e7e09
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-31 16:30:07 -0500

    wired things up in program.cs

--- EasyTodoListApp/Program.cs
+++ EasyTodoListApp/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using EasyTodoListApp.Infrastructure.DatabaseContext;
+using EasyTodoListApp.Infrastructure.Repository;
 
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services
+    .AddDbContext<EasyTodoListAppDbContext>()
+    .AddScoped<ITodoRepository, TodoRepository>()
+    .AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly))
+    .AddEndpointsApiExplorer();
+WebApplication app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();

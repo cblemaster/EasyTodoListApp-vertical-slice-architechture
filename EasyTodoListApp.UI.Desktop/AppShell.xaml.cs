c24bc1c1e6f6c25e58644483e32223deefd5d857
Brian LeMaster <brian.lemaster@outlook.com>
2025-01-02 16:02:19 -0500

    added desktop ui project (MAUI)

---
+++ EasyTodoListApp.UI.Desktop/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyTodoListApp.UI.Desktop
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

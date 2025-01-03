589b0ba746db3639a44159a66c7674dc2f35c30f
Brian LeMaster <brian.lemaster@outlook.com>
2025-01-02 20:34:59 -0500

    added models for http data service to consume in MAUI project

---
+++ EasyTodoListApp.UI.Desktop/Models/CreateTodoCommand.cs
@@ -0,0 +1,4 @@
+﻿
+namespace EasyTodoListApp.UI.Desktop.Models;
+
+internal record CreateTodoCommand(string Description, DateOnly? DueDate, bool IsImportant, bool IsComplete);
---
+++ EasyTodoListApp.UI.Desktop/Models/UpdateTodoCommand.cs
@@ -0,0 +1,4 @@
+﻿
+namespace EasyTodoListApp.UI.Desktop.Models;
+
+internal record UpdateTodoCommand(string Description, DateOnly? DueDate, Guid Id);

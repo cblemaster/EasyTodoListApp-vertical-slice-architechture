2549d1b2a389575fa241ba62c49b6dc295b4d5b3
Brian LeMaster <brian.lemaster@outlook.com>
2025-01-08 19:48:18 -0500

    added validation to create todo page model in maui project

--- EasyTodoListApp.UI.Desktop/PageModels/CreateTodoPageModel.cs
+++ EasyTodoListApp.UI.Desktop/PageModels/CreateTodoPageModel.cs
@@ -3,10 +3,11 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using EasyTodoListApp.UI.Desktop.Services;
 using EasyTodoListApp.UI.Desktop.Services.Validation;
+using EasyTodoListApp.UI.Desktop.Services.Validation.Rules;
 
 namespace EasyTodoListApp.UI.Desktop.PageModels
 {
-    public partial class CreateTodoPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class CreateTodoPageModel : PageModelBase
     {
         [ObservableProperty]
         public ValidatableObject<string> _description = new() { Value = string.Empty };
@@ -16,6 +17,19 @@ namespace EasyTodoListApp.UI.Desktop.PageModels
         public bool _isImportant;
         [ObservableProperty]
         public bool _isComplete;
+        [ObservableProperty]
+        
+        [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
+        private bool _isValid;
+        
+        
+
+        public CreateTodoPageModel(IDataService dataService) : base(dataService)
+        {
+            Description.Validations.Add(new StringIsNotNullOrEmptyRule<string> { ValidationMessage = "Description is required." });
+            Description.Validations.Add(new StringIsNotExclusivelyWhitespaceRule<string> { ValidationMessage = "Description cannot be exclusively whitespace." });
+            Description.Validations.Add(new StringDoesNotExceedLengthOfOneHundredRule<string> { ValidationMessage = "Description must be 100 characters or fewer." });
+        }
 
         public override string About => "page for creating todos";
         public override void LoadDataAsync() { }
@@ -23,7 +37,10 @@ namespace EasyTodoListApp.UI.Desktop.PageModels
         [RelayCommand]
         public async Task CancelAsync() => await Shell.Current.Navigation.PopModalAsync();
 
-        [RelayCommand]
+        [RelayCommand(CanExecute = nameof(IsValid))]
         public async Task SaveAsync() { }
+
+        [RelayCommand]
+        public void ValidateDescription() => IsValid = Description.Validate();
     }
 }

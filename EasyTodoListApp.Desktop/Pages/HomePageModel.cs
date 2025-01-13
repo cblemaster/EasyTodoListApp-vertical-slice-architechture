67b74d2e35337204ad1face5f9925ed1b9f1a402
Brian LeMaster <brian.lemaster@outlook.com>
2025-01-12 20:50:24 -0500

    added pagemodel for home page in desktop project

---
+++ EasyTodoListApp.Desktop/Pages/HomePageModel.cs
@@ -0,0 +1,23 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EasyTodoListApp.Desktop.Models;
+using EasyTodoListApp.Desktop.Services;
+using System.Collections.ObjectModel;
+
+namespace EasyTodoListApp.Desktop.Pages;
+
+public partial class HomePageModel(IDataService dataService) : ObservableObject
+{
+    private readonly IDataService _dataService = dataService;
+
+    [ObservableProperty]
+    public ReadOnlyCollection<TodoDTO> _todos = new([]);
+
+    [RelayCommand]
+    public async Task LoadDataAsync()
+    {
+        IEnumerable<TodoDTO> response = await _dataService.GetAllTodosNotCompleteAsync();
+        Todos = response.ToList().AsReadOnly();
+    }
+}

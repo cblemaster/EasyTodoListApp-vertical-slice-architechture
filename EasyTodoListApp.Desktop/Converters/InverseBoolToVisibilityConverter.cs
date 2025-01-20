32028715b7f5ddff2575504a91a6b02a3291da11
Brian LeMaster <brian.lemaster@outlook.com>
2025-01-20 18:53:58 -0500

    added value converters in desktop project

---
+++ EasyTodoListApp.Desktop/Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace EasyTodoListApp.Desktop.Converters;
+
+public class InverseBoolToVisibilityConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is bool b && b ? Visibility.Collapsed : Visibility.Visible;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}

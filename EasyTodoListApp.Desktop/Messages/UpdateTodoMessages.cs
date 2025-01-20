b12cf58dc2e209d81e3b79be8c27f6d0fe1bfcc0
Brian LeMaster <brian.lemaster@outlook.com>
2025-01-20 14:52:26 -0500

    added update todo messages in desktop project

---
+++ EasyTodoListApp.Desktop/Messages/UpdateTodoMessages.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Windows;
+
+namespace EasyTodoListApp.Desktop.Messages;
+
+public static class UpdateTodoMessages
+{
+    private const string CAPTION = "Update todo";
+    private static readonly MessageBoxButton _button = MessageBoxButton.OK;
+
+    public static MessageBoxResult ShowUpdateTodoSucceededMessage(string message)
+    {
+        MessageBoxImage icon = MessageBoxImage.Information;
+        return MessageBox.Show(message, CAPTION, _button, icon);
+    }
+
+    public static MessageBoxResult ShowUpdateTodoFailedMessage(string message)
+    {
+        MessageBoxImage icon = MessageBoxImage.Error;
+        return MessageBox.Show(message, CAPTION, _button, icon);
+    }
+
+    public static MessageBoxResult ShowUpdateTodoErrorMessage(string error)
+    {
+        string message = $"Create todo failed with this error: {error}";
+        MessageBoxImage icon = MessageBoxImage.Error;
+        return MessageBox.Show(message, CAPTION, _button, icon);
+    }
+
+    public static MessageBoxResult ShowUpdateTodoValidationErrorMessage(string error)
+    {
+        string message = $"The following validation error(s) occured, and must be resolved before the todo can be updated: {error}";
+        MessageBoxImage icon = MessageBoxImage.Information;
+        return MessageBox.Show(message, CAPTION, _button, icon);
+    }
+
+    public static MessageBoxResult ShowTodoToUpdateNotFoundMessage()
+    {
+        string message = "Could not find to do to update!";
+        MessageBoxImage icon = MessageBoxImage.Error;
+        return MessageBox.Show(message, CAPTION, _button, icon);
+    }
+}
